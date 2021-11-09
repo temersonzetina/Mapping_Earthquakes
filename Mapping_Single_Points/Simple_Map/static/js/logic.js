3aa484822e697d475f9b6b24ee58f6121ce58f48
Anthony Emerson <anthonyemerson@Anthonys-MacBook-Air.local>
2021-11-08 20:47:50 -0800

    Adding single marker map

--- Mapping_Single_Points/Simple_Map/static/js/logic.js
+++ Mapping_Single_Points/Simple_Map/static/js/logic.js
@@ -1,12 +1,12 @@
 // Add console.log to check to see if our code is working.
-console.log("working");
+console.log("worrrking");
 
 // Create the map object with a center and zoom level. This is the geographical center of the US, FYI.
 // The "L.map" is saying "Leaflet, make me a map, placed in the right div tag.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
@@ -15,4 +15,10 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-
+//  Add a marker to the map for Los Angeles, California.
+L.circleMarker([34.0522, -118.2437], {
+    radius: 300,
+    color: 'black',
+    fillColor: '#ffd966',
+    fillOpacity: 0.5,
+}).addTo(map);
